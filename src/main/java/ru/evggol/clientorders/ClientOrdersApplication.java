a771957af21429a8bd29b780eb2888fc0fbec893
Eugenii <evggol@list.ru>
2025-11-07 17:17:21 +0300

    Первый коммит по ClientOrders

---
+++ src/main/java/ru/evggol/clientorders/ClientOrdersApplication.java
@@ -0,0 +1,13 @@
+package ru.evggol.clientorders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientOrdersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientOrdersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/evggol/clientorders/ClientOrdersApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.evggol.clientorders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientOrdersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
